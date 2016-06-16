16f03634d8fbc983a5510bb2daae4688254ed6c6
MattHodge <matthodge@gmail.com>
2016-06-16 07:47:19 +0200

    working version

--- DSCResources/HubotPrerequisites/HubotPrerequisites.schema.psm1
+++ DSCResources/HubotPrerequisites/HubotPrerequisites.schema.psm1
@@ -4,15 +4,32 @@
     (
         [Parameter()]
         [ValidateNotNullOrEmpty()]
-        [String] $ChocolateyInstallPath = 'C:\choco'
+        [String] $ChocolateyInstallPath = 'C:\choco',
+
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [PSCredential] $PsDscRunAsCredential,
+
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [string] $GitDownloadUri
     )
 
     # Import the module that defines custom resources
     Import-DscResource -Module cChoco
 
+
     cChocoInstaller installChoco
     {
         InstallDir = $ChocolateyInstallPath
+    }
+    
+    # need to run the git installer under a user account otherwise the install just hangs
+    cChocoPackageInstaller installGit
+    {
+        Name = "git"
+        DependsOn = "[cChocoInstaller]installChoco"
+        PsDscRunAsCredential = $PsDscRunAsCredential
     }
 
     cChocoPackageInstaller installnssm
@@ -21,16 +38,11 @@
         DependsOn = "[cChocoInstaller]installChoco"
     }
 
-    cChocoPackageInstaller installGit
-    {
-        Name = "git.install"
-        DependsOn = "[cChocoInstaller]installChoco"
-    }
-
     cChocoPackageInstaller installNode
     {
         Name = "nodejs.install"
         Version = "5.10.1"
         DependsOn = "[cChocoInstaller]installChoco"
-    }
+    }   
+
 }
--- examples/dsc_configuration.ps1
+++ examples/dsc_configuration.ps1
@@ -1,15 +1,35 @@
 ﻿configuration Hubot
-{
+{   
     Import-DscResource -ModuleName PSDesiredStateConfiguration
     Import-DscResource -Name MSFT_xRemoteFile -ModuleName xPSDesiredStateConfiguration
     Import-DscResource -ModuleName Hubot
 
     node $AllNodes.Where{$_.Role -eq "Hubot"}.NodeName
     {
-        Environment hubotslackadapter
+        # Create a user to install prereqs under and run the hubot service
+        User Hubot
+        {
+            UserName = $Node.HubotUserCreds.UserName
+            Password = $Node.HubotUserCreds
+            Ensure = 'Present'
+            PasswordNeverExpires = $true
+            PasswordChangeRequired = $false
+        }
+        
+        # Create a user to install prereqs under and run the hubot service
+        Group HubotUser
+        {
+            GroupName = 'Administrators'
+            MembersToInclude = $Node.HubotUserCreds.UserName
+            Ensure = 'Present'
+            DependsOn = "[User]Hubot"
+        }
+        
+        
+        Environment hubotadapter
         {
             Name = 'HUBOT_ADAPTER'
-            Value = 'slack'
+            Value = $Node.HubotAdapter
             Ensure = 'Present'
         }
 
@@ -23,13 +43,15 @@
         Environment hubotslacktoken
         {
             Name = 'HUBOT_SLACK_TOKEN'
-            Value = 'xoxb-XXXXXXXXXXX-XXXXXXXXXXXXXXXXXXXXXX'
+            Value = $Node.SlackAPIKey
             Ensure = 'Present'
         }
 
+        # Install the Prereqs using the same Hubot user
         HubotPrerequisites installPreqs
         {
             ChocolateyInstallPath = 'C:\choco'
+            PsDscRunAsCredential = $Node.HubotUserCreds
         }
 
         # Download the HubotWindows Repo
@@ -43,45 +65,57 @@
         Archive extractHubotRepo
         {
             Path = "$($env:Temp)\HubotWindows.zip"
-            Destination = "C:\myhubot"
+            Destination = $Node.HubotBotPath
             Ensure = 'Present'
             DependsOn = '[xRemoteFile]hubotRepo'
         }
 
         HubotInstall installHubot
         {
-            BotPath = 'C:\myhubot'
+            BotPath = $Node.HubotBotPath
             Ensure = 'Present'
             DependsOn = '[Archive]extractHubotRepo'
+
         }
 
         HubotInstallService myhubotservice
         {
-            BotPath = 'C:\myhubot'
-            ServiceName = 'Hubot_Bender'
-            BotAdapter = 'slack'
+            BotPath = $Node.HubotBotPath
+            ServiceName = "Hubot_$($Node.HubotBotName)"
+            BotAdapter = $Node.HubotAdapter
             Ensure = 'Present'
             DependsOn = '[HubotPrerequisites]installPreqs'
         }
 
         Service modifyHubotService
         {
-            Name = 'Hubot_Bender'
-            Description = 'Bender Hubot Bot'
+            Name = "Hubot_$($Node.HubotBotName)"
+            Description = "$($Node.HubotBotName) Hubot Bot"
             Ensure = 'Present'
             StartupType = 'Automatic'
             State = 'Running'
             DependsOn = '[HubotInstallService]myhubotservice'
+            Credential = $Node.HubotUserCreds
         }
     }
 }
 
+# Create Hubot Credentials (save having to enter them every time - don't do this for production!)
+$hubotUserPass = ConvertTo-SecureString 'MyPASSWORD!' -AsPlainText -Force
+$hubotUserCreds = New-Object System.Management.Automation.PSCredential ('Hubot', $hubotUserPass)
+
+
 $configData = @{
 AllNodes = @(
-    @{
-        NodeName = 'localhost';
-        PSDscAllowPlainTextPassword = $true
-        Role = 'Hubot'
+        @{
+            NodeName = 'localhost';
+            PSDscAllowPlainTextPassword = $true
+            Role = 'Hubot'
+            HubotUserCreds = $hubotUserCreds
+            SlackAPIKey = 'xoxb-XXXXXXXXX-XXXXXXXXXXXXXXXXXXXXX'
+            HubotAdapter = 'slack'
+            HubotBotName = 'bender'
+            HubotBotPath = 'C:\myhubot'
         }
     )
 }
